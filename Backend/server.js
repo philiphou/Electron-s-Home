b2b0e52248e434e57c1fbcb7c4d6dd15249a7e99
pengcheng-hou <hphilip168@gmail.com>
2022-09-30 17:30:33 -0400

    user backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from './routes/userRoutes.js'
 
 import connectDB from "./config/db.js";
 
@@ -11,8 +12,11 @@ import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 dotenv.config();
 
 connectDB();
+// middleware to parse json data
+app.use(express.json())
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 //
 app.use(notFound);
