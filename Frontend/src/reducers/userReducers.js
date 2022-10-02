aa338cecd31e2b31fe8fd47848eaade103060201
pengcheng-hou <hphilip168@gmail.com>
2022-10-01 20:12:43 -0400

    front end user reducers and actions

---
+++ Frontend/src/reducers/userReducers.js
@@ -0,0 +1,21 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+} from "../constants/userConstants";
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      const item = action.payload;
+      return { loading: true };
+
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+
+    case USER_LOGIN_FAIL:
+      return { loading: false, userInfo: action.payload };
+
+    default:
+      return state;
+  }
+};
--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -5,19 +5,25 @@ import {
   productDetailReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 const reducer = combineReducers({
   productList: productListReducer,
   productDetail: productDetailReducer,
   cart: cartReducer,
+  user: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 const preloadedState = {
   cart: {
-    cartItems: cartItemsFromStorage
+    cartItems: cartItemsFromStorage,
   },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 const store = configureStore({ reducer, preloadedState });
 export default store;
