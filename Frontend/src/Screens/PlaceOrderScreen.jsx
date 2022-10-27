cab04cd6bf9103212bfd8b2e1a3f0149078d2e09
pengcheng-hou <hphilip168@gmail.com>
2022-10-27 10:25:19 -0400

    place order price

--- Frontend/src/Screens/PlaceOrderScreen.jsx
+++ Frontend/src/Screens/PlaceOrderScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import {
   Row,
   Col,
@@ -6,17 +6,31 @@ import {
   Image,
   Button,
   ListGroupItem,
+  Card
 } from "react-bootstrap";
-import FormContainer from "../Components/FormContainer";
+
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate,Link } from "react-router-dom";
-import Loader from "../Components/Loader";
+import { Link } from "react-router-dom";
+
 import CheckoutStep from "../Components/CheckoutStep";
-import { saveShippingAddress } from "../actions/cartActions";
+
 import Message from "../Components/Message";
 
 const PlaceOrder = () => {
-  const cart = useSelector((state) => state.cart);
+  let cart = useSelector((state) => state.cart);
+  // get total price before tax and shipping
+  cart.itemsPrice= cart.cartItems.reduce((acc,cur)=>(acc+cur.price*cur.qty),0)
+  //  get shipping costs
+  cart.shippingPrice=cart.itemsPrice>50?0:15
+  //  get tax expenses
+  cart.taxPrice=cart.itemsPrice*0.10
+  //  get total price
+  cart.totalPrice = cart.itemsPrice+cart.shippingPrice+cart.taxPrice
+
+  const dispatch = useDispatch()
+  const placeOrderHandler=(e)=>{
+
+  }
   return (
     <>
       <CheckoutStep step1 step2 step3 step4></CheckoutStep>
@@ -50,12 +64,10 @@ const PlaceOrder = () => {
                             <Image src={x.image} alt={x.name} fluid rounded />
                           </Col>
                           <Col>
-                          <Link to={`/product/${x.product}`}>
-                            {x.name}
-                          </Link>
+                            <Link to={`/product/${x.product}`}>{x.name}</Link>
                           </Col>
                           <Col md={4}>
-                            {x.qty}x${x.price}=${x.qty*x.price}
+                            {x.qty}x${x.price}=${x.qty * x.price}
                           </Col>
                         </Row>
                       </ListGroupItem>
@@ -65,6 +77,47 @@ const PlaceOrder = () => {
               )}
             </ListGroupItem>
           </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <h2>Order Summary</h2>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${cart.itemsPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${cart.shippingPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${cart.taxPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${cart.totalPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  type="button"
+                   className="btn w-100"
+                  disabled={cart.cartItems.length === 0}
+                  onClick={placeOrderHandler}
+                >PLACE ORDER</Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </>
