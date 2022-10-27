5225877bedcfa15c5766cc120a45f022abdafceb
pengcheng-hou <hphilip168@gmail.com>
2022-10-27 10:44:05 -0400

    order add backend

---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,33 @@
+import Order from "../models/orderModel.js";
+import asyncHandler from "express-async-handler";
+//  create new order
+//  access: private
+//  route GET /api/order
+export const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No Order Items");
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createOrder = await order.save()
+    res.status(201).json(createOrder)
+  }
+});
